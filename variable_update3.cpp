be497385a012abceeede3f1f55aefdba4206e471
Zebiniso Roʻzmatova <zebinisorozmatova@gmail.com>
2026-03-05 22:52:32 +0500

    Create variable_update3.cpp

---
+++ variable_update3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int score = 50;   // o'zgaruvchi e'lon qilindi
+
+    score = 80;       // yangi qiymat berildi
+
+    cout << score;    // natija chiqariladi
+
+    return 0;
+}
