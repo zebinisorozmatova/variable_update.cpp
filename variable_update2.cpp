55cdf028b34ee56700b1514d6de4c8fc22dd3864
Zebiniso Roʻzmatova <zebinisorozmatova@gmail.com>
2026-03-05 22:51:57 +0500

    Create variable_update2.cpp

---
+++ variable_update2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 16;     // o'zgaruvchi yaratildi
+
+    age = 17;         // qiymati yangilandi
+
+    cout << age;      // ekranga chiqariladi
+
+    return 0;
+}
