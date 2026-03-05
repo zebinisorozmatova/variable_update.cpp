7a0253d6002d46a40f76c4f22aefecd9b6a09235
Zebiniso Roʻzmatova <zebinisorozmatova@gmail.com>
2026-03-05 22:52:16 +0500

    Create variable_update1.cpp

---
+++ variable_update1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number = 10;  // o'zgaruvchi e'lon qilindi
+
+    number = 30;      // qiymat o'zgartirildi
+
+    cout << number;   // qiymat chiqariladi
+
+    return 0;
+}
